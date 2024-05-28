37c38abb21680e09975da0dc5b179a3ce9e23645
zhenya51600 <zhenyashevchenko123@gmail.com>
2024-05-28 17:36:39 +0300

    [IMP] (develop): Imp functions linter format

--- app/main.py
+++ app/main.py
@@ -1,13 +1,43 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            } for error in errors if error["filename"] == file_path
+        ],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                } for error in errors
+            ],
+            "path": file_path,
+            "status": "failed" if errors else "passed"
+        }
+        for file_path, errors in linter_report.items()
+    ]
